235b645d3447f3bf5aff4d1f35ffca7e06a5c253
Sergi Tubert <sergi.tubert@primer-impacto.com>
2020-11-22 22:31:39 +0100

    Hide description on small screens

--- src/components/Home/SuggestionItem.tsx
+++ src/components/Home/SuggestionItem.tsx
@@ -12,9 +12,9 @@ export const SuggestionItem = ({ description, highlight, version, name }: Props)
     <Link to={`/${name}`}>
         <div className="bg-gray-200 border border-gray-300 mx-6 hover:bg-gray-100 cursor-pointer">
             <div className="flex justify-between p-2">
-                <div>
+                <div className="w-full">
                     <p dangerouslySetInnerHTML={{ __html: highlight }} />
-                    <p className="truncate overflow-ellipsis overflow-hidden">{description}</p>
+                    <p className="hidden lg:block truncate overflow-ellipsis overflow-hidden">{description}</p>
                 </div>
                 <div className="flex items-center">
                     <p>{version}</p>
