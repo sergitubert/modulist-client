0ec4ed484e7dd18934b453f8d35c39165ad402cf
Sergi Tubert <sergi.tubert@primer-impacto.com>
2020-11-23 19:59:26 +0100

    Lighter bg color

--- src/components/shared/Layout/Layout.tsx
+++ src/components/shared/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import Header from '../Header/Header';
 
 export const Layout: React.FC = ({ children }) => {
     return (
-        <div className="bg-gray-100 text-gray-900">
+        <div className="bg-gray-50 text-gray-900">
             <div className="min-h-screen">
                 <Header />
                 {children}
