7849e1d3932ab9fafdb3d72c7fa3eb26b04b4cb0
Sergi Tubert <sergi.tubert@primer-impacto.com>
2020-11-25 00:26:35 +0100

    Some ios pwa

--- src/components/shared/Layout/Layout.tsx
+++ src/components/shared/Layout/Layout.tsx
@@ -3,11 +3,15 @@ import Header from '../Header/Header';
 
 export const Layout: React.FC = ({ children }) => {
     return (
-        <div className="bg-gray-50 text-gray-900">
-            <div className="min-h-screen">
-                <Header />
-                {children}
+        <React.Fragment>
+            <div className="bg-gray-50 text-gray-900">
+                <div className="statusbar mt-6 md:mt-0" />
+                <div className="min-h-screen">
+                    <Header />
+                    {children}
+                </div>
             </div>
-        </div>
+        </React.Fragment>
+
     );
 }
