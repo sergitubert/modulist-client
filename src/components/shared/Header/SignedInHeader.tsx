b04acc7e16556d0dba1b61cba9c4d905cc148338
Sergi Tubert <sergi.tubert@primer-impacto.com>
2020-11-30 21:24:47 +0100

    User Account dropdown

--- src/components/shared/Header/SignedInHeader.tsx
+++ src/components/shared/Header/SignedInHeader.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
+import OutsideClickHandler from 'react-outside-click-handler';
 import { useHistory } from 'react-router-dom';
 import { User } from '../../../Models/User';
 import { userStore } from '../../../utils/cookie';
+import useBoolean from '../../../utils/useBoolean';
 import UserInfo from './UserInfo';
 
 const SignedInHeader = () => {
     const history = useHistory();
     const { user, setHasUser, setUser } = userStore()
+    const [userDropdownOpen, { toggle, off }] = useBoolean(false);
 
     useEffect(() => {
 
@@ -28,7 +31,21 @@ const SignedInHeader = () => {
 
     return (
         <React.Fragment>
-            { user && <UserInfo />}
+            { user &&
+                <div className="relative">
+                    <UserInfo onClick={toggle} />
+                    {userDropdownOpen &&
+                        <OutsideClickHandler onOutsideClick={off}>
+                            <div className="bg-white flex flex-col rounded-lg py-2 mt-2 shadow-md w-48 absolute right-0 ">
+                                <a href="" className="px-4 py-2 hover:bg-gray-50">Profile</a>
+                                <a href="" className="px-4 py-2 hover:bg-gray-50">My Lists</a>
+                                <a href="" className="px-4 py-2 hover:bg-gray-50">Contact</a>
+                            </div>
+                        </OutsideClickHandler>
+                    }
+                </div>
+            }
+
         </React.Fragment>
     );
 }
